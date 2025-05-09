ca69efe742835cdd36fb216b2fa0eea1d4cab307
joshhh7 <joshset03@gmail.com>
2025-05-09 20:04:11 +0200

    test temps

---
+++ src/ValidityTests/testTPS.c
@@ -0,0 +1,48 @@
+#include <flint/flint.h>
+#include <flint/fmpz_poly.h>
+#include <flint/fmpz.h>
+#include <flint/fmpq.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "../HeaderFiles/taylorShift_implem.h"
+#include "../HeaderFiles/functionsForTests.h"
+#include "../HeaderFiles/coeff_truncation.h"
+
+
+int main()
+{
+    fmpz_poly_t poly, TrueResult;
+    fmpz_poly_init(poly);
+    fmpz_poly_init(TrueResult);
+    
+    readPolyDATA(poly, 0, 60);
+
+    slong threshold = 256*2;
+    fmpz_poly_t* power_array;
+    slong levels = compute_power_array(&power_array, poly, threshold);
+    
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    iterative_taylor_shift_precompute(TrueResult, poly, threshold, power_array);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double tps = (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_nsec - start.tv_nsec) / 1e3;
+    printf("Tps 1 : %f\n", tps);
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    iterative_taylor_shift_precompute(TrueResult, poly, threshold, power_array);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    tps = (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_nsec - start.tv_nsec) / 1e3;
+    printf("Tps 2 : %f\n", tps);
+
+
+
+    //cleanup the precomputation
+    for (int i = 0; i < levels; i++)
+        fmpz_poly_clear(power_array[i]);
+    free(power_array);
+
+    fmpz_poly_clear(TrueResult);
+    return 0;
+}
